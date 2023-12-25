96f8cf9e509647c3a8c434e8137c1f00411b199d
Parchello <parchellos@gmail.com>
2023-12-25 02:25:48 +0200

    more styles

--- src/components/MovieSearchList/MovieSearchList.js
+++ src/components/MovieSearchList/MovieSearchList.js
@@ -1,9 +1,6 @@
-import {
-  FilmLi,
-  FilmList,
-  FilmName,
-} from 'components/TrendMovies/TrendMovies.styled';
-import { Link, useLocation } from 'react-router-dom';
+import { StyledLink } from 'components/TrendMovies/TrendMovies.styled';
+import { useLocation } from 'react-router-dom';
+import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 
 export default function MovieSearchList({ items }) {
   const location = useLocation();
@@ -12,25 +9,44 @@ export default function MovieSearchList({ items }) {
   return (
     <div>
       {items ? (
-        <FilmList>
+        <div
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'center',
+          }}
+        >
           {items.map(item => (
-            <FilmLi key={item.id}>
-              <Link to={`${item.id}`} state={{ from: location }}>
-                <img
-                  src={
+            <Card
+              key={item.id}
+              sx={{ maxWidth: 200, margin: 2, backgroundColor: 'grey' }}
+            >
+              <StyledLink to={`${item.id}`} state={{ from: location }}>
+                <CardMedia
+                  component="img"
+                  alt={item.title}
+                  height="300"
+                  width="200"
+                  image={
                     item.poster_path
                       ? imgBaseUrl + item.poster_path
                       : 'https://www.shutterstock.com/image-vector/no-image-available-vector-illustration-260nw-744886198.jpg'
                   }
-                  alt={item.title}
-                  width={300}
-                  height={450}
                 />
-                <FilmName> {item.title}</FilmName>
-              </Link>
-            </FilmLi>
+
+                <CardContent>
+                  <Typography
+                    variant="body2"
+                    component="div"
+                    textAlign={'center'}
+                  >
+                    {item.title}
+                  </Typography>
+                </CardContent>
+              </StyledLink>
+            </Card>
           ))}
-        </FilmList>
+        </div>
       ) : (
         <p>Loading, please wait.....</p>
       )}
